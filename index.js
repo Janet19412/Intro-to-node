00d44ac9390cc307ad41db573f7c9de722424e75
yaowenjing16 <yaowenjin@yaowenjingdeMacBook-Air.local>
2021-08-17 15:11:56 +0800

    save initial change

---
+++ index.js
@@ -0,0 +1,8 @@
+//jshint esversion:6
+// the comment will tell the linter that I'm actually using ES Version 6. because the const is only available in ES6.
+
+var supervillains = require("supervillains");
+
+supervillainsName = supervillains.random();
+
+console.log(supervillainsName);
